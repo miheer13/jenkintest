981a1f7ff752baf7655b87bc3cef429db48d21f6
teetee <80090371+miheer13@users.noreply.github.com>
2023-02-20 16:21:33 +0530

    test6

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ class HelloWorld{
 	public static void main(String args[]){
 		System.out.println("Hello Java");
 		System.out.println("test");
-		System.out.println("tessst");
-		System.out.println("tessfgsgfsst");
+		System.out.println("tessst3");
 	}
 }
